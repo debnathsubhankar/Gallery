aa093876e4d35b7183928e630a4de81aa0c73bbb
debnathsubhankar <subhankardebnat923262@gmail.com>
2023-10-07 02:02:03 +0530

    new code

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Gallery from "./Component/Gallery";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
 }
 
 export default App;
---
+++ webapp/src/Component/Gallery.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="container">
+      <h1>My Gallery</h1>
+      <div className="container-fluid">
+        <i>
+          <button>All Item</button>
+        </i>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
